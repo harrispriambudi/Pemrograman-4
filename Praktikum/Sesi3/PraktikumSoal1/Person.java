ddd3f57d4cbaf4b50e1049103e6cb4d91f6690c5
Harris Priambudi <harris@harris-pc.(none)>
2011-10-07 13:46:22 +0700

    praktikum

--- Praktikum/Sesi3/PraktikumSoal1/Person.java
+++ Praktikum/Sesi3/PraktikumSoal1/Person.java
@@ -1,11 +1,11 @@
-public class Person
+public class Person // deklarasi public class bernama "Person"
 	{
-	public static void main(String[] args)
+	public static void main(String[] args) // deklarasi 
 		{
-		int value = 10;
-		char x;
-		x = 'A';
-		System.out.println(value);
-		System.out.println("The value of x=" + x);
+		int value = 10; // deklarasi variabel "value" yang berjenis integer dan mempunyai nilai '10'
+		char x; // deklarasi variabel "x" yang berjenis char
+		x = 'A'; // nilai dari variabel "x" adalah "A"
+		System.out.println(value); // perintah untuk menampilkan nilai dari variabel "value"
+		System.out.println("The value of x=" + x); // perintah untuk menampilkan "the value of x" = + x      ( + x arinya menampilkan nilai dari x )
 		}
 	}
---
+++ Praktikum/Sesi3/PraktikumSoal3/Person.java
@@ -0,0 +1,14 @@
+public class Person // mendeklarasikan sebuah class dengan nama Person yang bersifat public 
+	{
+	public static void main (String[] args) /*mendeklarasikan suatu method bernama main yang akan
+	 pertama diakses ketika program dieksekusi , method ini menggunakan keyword void sehingga tidak mengembalikan nilai apapun 
+	 dan dapat diakses dari dalam maupun luar karena bersifat public*/
+		{
+		int ourfirstnumber = 10; //mendeklarasikan suatu variable dengan nama ourfirstnumber dengan tipe data integer dan nilai 10
+		double ourfirstdouble = 10; // mendeklarasikan suatu variable dengan nama outfirstdouble dengan tipe data double dan bernilai 10
+
+		System.out.println(ourfirstnumber); // menampilkan nilai dari variable outfirstnumber
+		System.out.println(ourfirstdouble); // menampilkan nilai dari variable outfirstdouble
+		}
+	}
+	
