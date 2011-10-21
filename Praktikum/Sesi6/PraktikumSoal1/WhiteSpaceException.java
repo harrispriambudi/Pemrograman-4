9e45f9759e641ae3b4196a5704bad330cff14404
Harris Priambudi <hrrzorcozzy@gmail.com>
2011-10-21 11:26:51 +0700

    perpustakaan:

---
+++ Praktikum/Sesi6/PraktikumSoal1/WhiteSpaceException.java
@@ -0,0 +1,13 @@
+public class WhiteSpaceException extends Exception
+	{
+	public WhiteSpaceException()
+			{
+			super("Terjadi kesalahan : 'tidak boleh ada spasi / whitespace'");
+			}
+			
+	public WhiteSpaceException(String text)
+			{
+			super("Terjadi kesalahan : 'tidak boleh ada spasi / whitespace', text : " + text);
+			}
+	}
+	
